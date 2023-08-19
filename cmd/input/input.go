6f017257b0187d972cb46cdf4f855525a1ac7541
Vadim Gorbachev <vadim.gorbachev.dev@gmail.com>
2023-08-19 14:49:12 +0800

    add logo, move input message to input module

--- cmd/input/input.go
+++ cmd/input/input.go
@@ -3,10 +3,12 @@ package input
 import (
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/ProstoyVadila/goproj/internal/models"
+	"github.com/ProstoyVadila/goproj/pkg/output"
 )
 
 // Get tries to get information about the project from input.
 func Get(configExists bool, conf models.GlobalConfig) (*models.Setup, error) {
+	output.Info("Let's start!")
 	setup := new(models.Setup)
 	var useConfig bool
 
